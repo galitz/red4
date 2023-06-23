05c8c69d5256fa4cc5dd9d6515bbb2ad6deb5bbb
Geoff Galitz <ggalitz@shutterstock.com>
2023-06-23 17:35:54 +0000

    add 403-bypass setup path

--- red4.sh
+++ red4.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+
+# setup
+which 403-bypass.sh || echo "PATH to 403 bypass:" ; read
+PATH=$PATH:$REPLY
+
+which 403-bypass.sh || echo "Nope." ; exit 2
+
+for i in `cat files.txt`
+do
+## things
+done
+
