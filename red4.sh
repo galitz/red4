6dd9c69166723118981b3af8a3cff03d702fa6a4
Geoff Galitz <ggalitz@shutterstock.com>
2023-06-23 17:54:20 +0000

    build it out

--- red4.sh
+++ red4.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 
+
 # setup
+echo "URL:" ; read
+URL=$REPLY
+
 which 403-bypass.sh || echo "PATH to 403 bypass:" ; read
 PATH=$PATH:$REPLY
+
+gobuster --exclude-length 134 -w /usr/share/wordlists/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt
+-u $URL
 
 which 403-bypass.sh || echo "Nope." ; exit 2
 
 for i in `cat files.txt`
 do
-## things
+## do things
+  403-bypass.sh --exploit -u $URL
 done
 
